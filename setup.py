297595002b95c8210161c61dd69ed3fc973954d4
Juned Memon <junaid18183@gmail.com>
2014-02-28 03:04:19 -0800

    Bumpled to Version 0.1.2  now setup installs the Haproxy/keepalived

--- setup.py
+++ setup.py
@@ -1,10 +1,54 @@
 from distutils.core import setup
 from glob import glob
-import os,sys
+import platform,sys
+
+##################################################################################
+#This Portion tries to to install the required packages on Ubuntu and Centos/Redhat Based Systems
+packages = ['keepalived','haproxy']
+if platform.system() == 'Linux':
+        name=platform.dist()[0]
+        if name == 'Ubuntu':
+		import apt
+		cache = apt.cache.Cache()
+		cache.update()
+		for package in packages:
+		        pkg = cache[package]
+		        if pkg.is_installed:
+                		print "%s is already installed" %(package)
+		        else:
+                		pkg.mark_install()
+
+		try:
+		        cache.commit()
+		except Exception, arg:
+		        print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
+
+#If System is Centos/Redhat use YUM
+
+        elif name =='centos' or  name == 'redhat' :
+		import yum
+		yb=yum.YumBase()
+		#yb.conf.cache = 1
+		#installed = yb.rpmdb.returnPackages()
+		#print installed
+		for package in packages:
+	        	if yb.rpmdb.searchNevra(name=package):
+        	        	print "%s is installed" %(package)
+		        else:
+        		        print "Installing %s" %(package)
+                		yb.install(name=package)
+	                	yb.resolveDeps()
+		       	        yb.buildTransaction()
+			yb.processTransaction()
+else:
+	print "This system does not seems to be Linux, so exiting"
+
+##################################################################################
+
 
 setup(
     name='HALB',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/junaid18183/HALB',
     author='Juned Memon',
     author_email='junaid18183@gmail.com',
@@ -14,9 +58,6 @@ setup(
     scripts=glob("bin/*"),
     include_package_data=True,
     package_dir={'halb':'halb'},
-    #data_files=[('config',['conf/config.py'])], #This installs in /usr/config
-    #data_files=['conf/config.py'],  #This installs in /usr
-    #data_files=[(os.path.join(sys.prefix, 'juned', 'halb'), glob("conf/*"))],
     data_files=[
 		('/etc/HALB', ['conf/halb.conf']),
 		('/etc/HALB/DATA/dat', ['example/dat/example.vig','example/dat/real.dat','example/dat/vip.dat']),
